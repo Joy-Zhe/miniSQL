4bf2a59126d7c3deb5db25f12711e88f6b51123c
菜鸭 <97238476+j-yi-11@users.noreply.github.com>
2023-06-15 12:05:37 +0800

    Update table_page.h
    header

--- src/include/page/table_page.h
+++ src/include/page/table_page.h
@@ -50,7 +50,7 @@ class TablePage : public Page {
 
   bool MarkDelete(const RowId &rid, Transaction *txn, LockManager *lock_manager, LogManager *log_manager);
 
-  bool UpdateTuple(const Row &new_row, Row *old_row, Schema *schema, Transaction *txn, LockManager *lock_manager,
+  RetState UpdateTuple(const Row &new_row, Row *old_row, Schema *schema, Transaction *txn, LockManager *lock_manager,
                    LogManager *log_manager);
 
   void ApplyDelete(const RowId &rid, Transaction *txn, LogManager *log_manager);
